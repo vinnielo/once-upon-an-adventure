9cbf9d9abbd9de99635ff12021150e6152582f01
Vinnie Lopez <vinnielo01@gmail.com>
2024-04-05 12:46:51 -0700

    fixing routes

--- server/server.js
+++ server/server.js
@@ -14,8 +14,8 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "build")));
 
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build/index.html"));
   });
 }
 // Add routes, both API and view
