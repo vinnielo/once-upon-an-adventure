2e6d0b3bcec5864aa67231ac9bd0ad958dfba8b3
Vinnie Lopez <vinnielo01@gmail.com>
2024-04-05 09:22:09 -0700

    fixing proxy

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-import store from './config/store'
+import store from "./config/store";
 
-ReactDOM.render(<Provider store={store}>
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-</Provider>, document.getElementById('root'));
+  </Provider>,
+  document.getElementById("root")
+);
