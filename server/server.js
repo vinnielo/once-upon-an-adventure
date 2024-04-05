002035220ba421c1dcf361cb20ee189a6d5b98c5
Vinnie Lopez <vinnielo01@gmail.com>
2024-04-05 13:06:57 -0700

    fixing routes

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 // Serve up static assets (usually on heroku)
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/build/index.html"));
